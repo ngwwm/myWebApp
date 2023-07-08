268cdebeb1d628fa57e0230b2df01859ebba391f
Martin Ng <Martin.Ng@theaccessgroup.com>
2023-07-08 16:14:01 +0100

    Added Hangfire

--- myWebApp/Default.aspx.designer.cs
+++ myWebApp/Default.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace myWebApp
     {
 
         /// <summary>
+        /// txtWebhookURL control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtWebhookURL;
+
+        /// <summary>
         /// btnUnhandled control.
         /// </summary>
         /// <remarks>
@@ -31,5 +40,14 @@ namespace myWebApp
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnHandled;
+
+        /// <summary>
+        /// btnWebhook control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnWebhook;
     }
 }
